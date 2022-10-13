66cdd8877689f5bc0b334240a78cf5ea26eb386e
Wecreds <wecredsbr@gmail.com>
2022-10-13 14:44:29 -0300

    if god wants, finally done

--- script.js
+++ script.js
@@ -10,28 +10,34 @@ for (let btn of btnNumeros) {
 }
 
 function cliqueNumero(event) {
-        if (isNaN(visor.innerHTML)) {
+    if (isNaN(visor.innerHTML)) {
+        if (event.type === 'click'){
             visor.innerHTML = event.target.innerHTML;
+        } else {
+            visor.innerHTML = event.key;
         }
-        else {
-            visor.innerHTML = visor.innerHTML + event.target.innerHTML;
-        }
-
+    } else {
+    if (event.type === 'click'){
+        visor.innerHTML = visor.innerHTML + event.target.innerHTML;
+    } else {
+        visor.innerHTML = visor.innerHTML + event.key;
+    }
 }
-
+}
 btnPonto.addEventListener('click', cliquePonto);
 
 function cliquePonto(event) {
-let valorVisor = Number(visor.innerHTML);
-    if (!isNaN(visor.innerHTML+'.')) {
+    let valorVisor = Number(visor.innerHTML);
+    if (!isNaN(visor.innerHTML + '.')) {
         visor.innerHTML = visor.innerHTML + event.target.innerHTML;
     }
 }
 
 
 
-function reset(){
-        visor.innerHTML = ""; 
+function reset(event) {
+    visor.innerHTML = "";
+
 }
 
 for (let i of operador) {
@@ -46,7 +52,7 @@ function cliqueOperador(event) {
 
     if (!isNaN(visor.innerHTML)) {
         if (valorSalvo === undefined) {
-            
+
             valorSalvo = Number(visor.innerHTML);
         }
         else {
@@ -54,33 +60,57 @@ function cliqueOperador(event) {
         }
     }
 
-    operacaoSalva = event.target.innerHTML;
-    visor.innerHTML = event.target.innerHTML;
+    if (event.type === 'click') {
+        operacaoSalva = event.target.innerHTML;
+        visor.innerHTML = event.target.innerHTML;
+    } else {
+        operacaoSalva = event.key;
+        visor.innerHTML = event.key;
+    }
 }
+
+
 
 bntIgual.addEventListener('click', calcular);
 
 function calcular(event) {
-    if (valorSalvo != undefined && operacaoSalva != undefined && !isNaN(visor.innerHTML)){
+    if (valorSalvo != undefined && operacaoSalva != undefined && !isNaN(visor.innerHTML)) {
         visor.innerHTML = calculoOperacao(valorSalvo, operacaoSalva, Number(visor.innerHTML));
         valorSalvo = undefined;
         operacaoSalva = undefined;
     }
-    
+
 }
 
 function calculoOperacao(valor1, operacao, valor2) {
-    
-    if(operacao === "+") {
-        return valor1+valor2
+
+    if (operacao === "+") {
+        return valor1 + valor2
     }
     else if (operacao === "-") {
-        return valor1-valor2
+        return valor1 - valor2
     }
     else if (operacao === "/") {
-        return valor1/valor2
+        return valor1 / valor2
     }
     else if (operacao === "*") {
-        return valor1*valor2
+        return valor1 * valor2
+    }
+}
+
+let body = document.getElementsByTagName("body")[0];
+
+body.addEventListener(`keydown`, pressionou_tecla);
+
+function pressionou_tecla(event) {
+
+    if (event.keyCode === 13) {
+        calcular(event);
+    } else if (event.key === '+' || event.key === '-' || event.key === '/' || event.key === '*') {
+        cliqueOperador(event);
+    } else if (event.key === 'Backspace' || event.key === 'Escape') {
+        reset(event);
+    } else if (event.keyCode == '49' || event.keyCode == '50' || event.keyCode == '51' || event.keyCode == '52' || event.keyCode == '53' || event.keyCode == '54' || event.keyCode == '55 ' || event.keyCode == '56' || event.keyCode == '57' || event.keyCode == '48') {
+        cliqueNumero(event);
     }
 }
